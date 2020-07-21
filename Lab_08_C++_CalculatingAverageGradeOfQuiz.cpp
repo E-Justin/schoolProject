f6f2dabd027de5fb758d5d538d91a1fafdec09b8
E-Justin <67754579+E-Justin@users.noreply.github.com>
2020-07-21 10:10:32 -0500

    Update Lab_08_C++_CalculatingAverageGradeOfQuiz.cpp
    updated program.. it works now, but my grade is always displaying 10%

--- Lab_08_C++_CalculatingAverageGradeOfQuiz.cpp
+++ Lab_08_C++_CalculatingAverageGradeOfQuiz.cpp
@@ -1,5 +1,4 @@
 // Lab 08
-
 // This program should calculate the average score of a test after user inputs answers.
 
 #include <iostream>
@@ -8,47 +7,44 @@ using namespace std;
 
 const int SIZE = 10;	// array size
 int correctAnswers[SIZE] = {5, 22, 35, 0, 16, 28, 4, 3, 42, 52};	// correct answers stored in array
-void numberCorrect(int);	// function prototype
+void numberCorrect(int, int);	// function prototype
 void inputAnswers(int[]);	// function prototype
 int givenAnswers[SIZE];	// Array initialization
 int index = 0;
 
 
-int main()
-{
-	inputAnswers(givenAnswers);	// calling function to get answers from user
-	
-	numberCorrect(correctAnswers, givenAnswers); // calling function
-	
-	
-		
-	
-	return 0;
-}
 
 void inputAnswers(int givenAnswers[])			// function definition
 {
 	
-	for (int index = 0; index < 10; index++)
+	for (int index = 0; index < 10; index++) // initialize loop to accept answers 1-10 from user
 	{
-		int questionNumber = index + 1;
+		int questionNumber = index + 1;	// start at question number 1
 		cout << "Please enter your answer to problem number " << questionNumber << endl;	// prompts user to enter answers to questions 1-10.
 		cin >> givenAnswers[index];		// answers the user inputs are stored in the givenAnswers[index] array
 		
 	}
 }
-int numberCorrect(int correctAnswers[], int givenAnswers[])
+int numberCorrect(int correctAnswers[], int givenAnswers[]) // function definition with two arrays as arguments
 {
-	int numRight = 0;
-	for (int index = 0; index < 10; index++);
+	int numRight = 0;	// initialize numRight to 0
+	for (int index = 0; index < 10; index++);	// initialize loop to cycle through both arrays
 	{
-		if (correctAnswers[index] = givenAnswers[index])
+		if (correctAnswers[index] = givenAnswers[index])	// compares givenAnswers to correct answers
 		{
-			numRight++;
+			numRight++;										// if answers are the same, the program adds 1 to numRight
 		}
-		
 	}
-	cout << "Your quiz grade is " << numRight * 10 << "%"; 
+	cout << "Your test grade is " << numRight * 10 << "%";	// display test grade as a percent
+	return 0;
+}
+int main()
+{
+	inputAnswers(givenAnswers);	// calling function to get answers from user
+
+	numberCorrect(correctAnswers, givenAnswers); // calling function to compare arrays, which inputs numRight into algorithm to calculate average grade as a percent.
+
+	return 0;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
