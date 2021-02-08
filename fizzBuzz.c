a75425df44bc9d17211ac055a28b0591b334abad
E-Justin <67754579+E-Justin@users.noreply.github.com>
2021-02-08 13:17:55 -0600

    Create fizzBuzz.c

---
+++ fizzBuzz.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+
+int main(void)
+{
+	int i = 1;
+
+
+	while (i <= 100)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("%d : FizzBuzz\n", i);
+			
+		}
+		else if (i % 3 == 0)
+		{
+			printf("%d : Fizz\n", i);
+			
+		}
+		else if (i % 5 == 0)
+		{
+			printf("%d : Buzz\n", i);
+			
+		}
+		else
+		{
+			printf("%d\n", i);
+		}
+		
+			i++;
+		
+	}
+	return 0;
+}
