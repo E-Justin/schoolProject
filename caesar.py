21e7e435c413fd71caa920ed999a5340efc6dde0
E-Justin <67754579+E-Justin@users.noreply.github.com>
2021-02-10 07:10:29 -0600

    Update caesar.py
    It works now.

--- caesar.py
+++ caesar.py
@@ -1,15 +1,19 @@
-"""This program should:
+This program should:
 * accept a string from the user
 * shift the characters in the string by +5
 * output the encrypted string to the screen """
 
 userInput = str(input("Please enter a sentence to be encrypted"))
 print("The original string you typed in is :\n",userInput)
+encryptedString = " "
 
 
-encryptedString = ' '
 index = 0
 while userInput[index] != "":
-    encryptedChar = ord(userInput[index] + 5) #encrypts the string, one character at a time by adding 5 to the ASCII value
-    encryptedString[index] = encryptedChar
-    index += 1
+    encryptedChar = ord(userInput[index]) #converts each character to its ASCII equivent 
+    encryptedChar = encryptedChar + 5 # encrypts each character by adding 5 to its ASCII value
+    encryptedString += str(encryptedChar) #concatenates the encrypted string one character at a time to the encryptedString variable
+    index += 1 #iterates through the loop
+    
+
+print(encryptedString)
