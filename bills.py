bf6108d997c641d968f49430b2dda4902dffb65c
E-Justin <67754579+E-Justin@users.noreply.github.com>
2021-02-21 09:42:38 -0600

    Create bills.py
    program is starting to do what it should: 
    ***error***as of now, it adds the exit sequence '123456' to person1's total

---
+++ bills.py
@@ -0,0 +1,33 @@
+
+# program should add up the bills for person 1 and 2 separately
+# find the difference between the two people's bills for the month
+# divide the difference by two
+# add the difference, divided by two to the person who paid the least amount in bills
+# display the amount owed to which person
+count = 0
+index = 0
+person1 = 0.0
+person2 = 0.0
+temp1 = 0.0
+temp2 = 0.0
+bill = 0
+
+
+while bill != 123456:
+    bill = float(input("Enter a bill amount that person1 paid, or press 123456 to move on to Kristen's stuff"))
+    person1 += bill
+    print("Person1's current bills total for the month:", round(person1,2))
+    if bill == 123456:
+        break
+
+print("Person1's total for bills this month is: $", round(person1,2))
+    
+
+while bill != 9876:
+    bill = float(input("Enter a bill amount that person2 paid, or press 9876 to "))
+    person2 += bill
+    print("Person2's current bills total for the month:", round(person2, 2))
+    if bill == 9876:
+        break
+
+print("Person2's total for bills this month is: $", round(person2, 2))
