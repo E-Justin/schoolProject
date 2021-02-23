1ed2a1c3274df265b5ed84725f31477889ca978a
E-Justin <67754579+E-Justin@users.noreply.github.com>
2021-02-23 09:23:09 -0600

    Create lab_bills.py

---
+++ lab_bills.py
@@ -0,0 +1,24 @@
+user1Input = [0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.]
+user2Input = [0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.]
+
+user1Total = float
+user2Total = float
+
+index = 0
+count = 0
+
+
+selection = str(input("Would you like to enter bills paid by user1? y/n"))
+
+while selection != 'n':
+    user1Input[index] = str(input("Enter a bill amount that user1 paid this month"))
+    selection = str(input("Would you like to enter more bills paid by user1? y/n"))
+    index = index +1 
+
+print("User1 paid the following amounts:")
+while count < index:
+    print(user1Input[count])
+    user1Total = user1Total + float(user1Input[count])
+    count = count + 1
+
+print("User1Total =", user1Total)
