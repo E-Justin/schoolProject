546a7b7e955da76bbdc6ecc9e6efaf4fd1eba22c
E-Justin <67754579+E-Justin@users.noreply.github.com>
2021-03-01 13:39:00 -0600

    Update lab_bills.py

--- lab_bills.py
+++ lab_bills.py
@@ -31,7 +31,7 @@ while selection != 'n': # while selection is not n
 
 print(user1Name + " paid the following bills with these amounts:")
 while count < index: # displays each amount entered for user1
-    print("%s : $ %s ", user1Bills[count], user1Input[count]) # prints the bill name and the amount
+    print("%s : $ %s" % (user1Bills[count], user1Input[count])) # prints the bill name and the amount
     user1Total = user1Total + float(user1Input[count]) # adds up each element of the array to get the total amount bills paid for user1
     count = count + 1 # increases count to move to the next position in the array
 
@@ -42,14 +42,14 @@ print(user1Name + "'s Total =$", round(user1Total,2)) # displays user1's total
 selection2 = str(input("Would you like to enter bills paid by " + user2Name + "? y/n")) # selection2 = y/n
 
 while selection2 != 'n':
-    user2Bills[index] = str(input("What is the name of the bill that you are about to enter?")) # stores each bill name in array to display later
-    user2Input[index] = str(input("How much did " + user2Name + " pay for " + user2Bills[index] + " this month?")) # stores user input in an array, starting at 0
+    user2Bills[index2] = str(input("What is the name of the bill that you are about to enter?")) # stores each bill name in array to display later
+    user2Input[index2] = str(input("How much did " + user2Name + " pay for " + user2Bills[index2] + " this month?")) # stores user input in an array, starting at 0
     selection2 = str(input("Would you like to enter more bills paid by " + user2Name + "? y/n")) # continue, or no?
     index2 = index2 +1 
 
 print(user2Name + " paid the following amounts:")
 while count2 < index2: #displays each amount entered for user2
-    print("%s : $ %s ", user2Bills[count2], user2Input[count2]) # prints the bill name and the amount
+    print("%s : $ %s " % (user2Bills[count2], user2Input[count2])) # prints the bill name and the amount
     user2Total = user2Total + float(user2Input[count2]) # sums up the elements in the array to find total bills paid for user2
     count2 = count2 + 1
 
@@ -68,6 +68,7 @@ if user2Total > user1Total: # if user2 paid more in bills
 
 if user1Total == user2Total: # if users paid the same amount
     print("You each paid the same amount")
+
 
 
 
