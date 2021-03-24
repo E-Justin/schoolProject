1ec9414773d203e947cab4319c577b65f7a15202
E-Justin <67754579+E-Justin@users.noreply.github.com>
2021-03-24 11:24:02 -0500

    Update oldPeopleText.py

--- oldPeopleText.py
+++ oldPeopleText.py
@@ -3,22 +3,13 @@
 Tells the user what the program does                                            ***
 Allows the user to enter a text shortcut, such OMG, LOL, IMHO                   ***
 Provides the English definition of the text shortcut back to the screen.        ***
-Provide an error message if the entered text is not found.
-Can be run until the user enters the character 'x'
+Provide an error message if the entered text is not found.                      ***
+Can be run until the user enters the character 'x'                              ***
 Use a dictionary to hold the text shortcuts and definitions.  Implement at least 20 of the text definitions found at https://www.smart-words.org/abbreviations/text.html.
 
 Create your own menu and user interface from the command line.  Also, remember that we are coding in a professional environment, so keep it professional, TYVM.
 
 """
-print("This program allows the user to enter a text shortcut (LOL, OMG, IMHO)") # Tells the user what the program does
-print("and provides the English definition of the text shortcut.")
-
-input = str((input("Please enter a text shortcut (OMG, LOL, IMHO) to see its English Definition"))) # stores shortcut in variable
-
-
-
-
-
 dictionary = {
     "LOL": "Laugh Out Loud",
     "OMG": "Oh My Gosh",
@@ -40,12 +31,18 @@ dictionary = {
     "IOW": "In other words",
     "HF": "Have Fun",
     "FYI": "For your information"
-
-
 }
+print("This program allows the user to enter a text shortcut (LOL, OMG, IMHO)") # Tells the user what the program does
+print("and provides the English definition of the text shortcut.")
 
-print("The definition of %s is:" % input) # prints the shortcut entered by the user
-print(dictionary.get(input)) # prints the definition of the shortcut entered by the user.
+userInput = str
 
-"""print(len(dictionary)) # gets length
-print(dictionary.get("LOL")) # returns definition of key entered in quotes"""
+while userInput != 'x':
+    userInput = str(input("Please enter a text shortcut to see its English Definition or press 'x' to exit the program"))
+    if userInput == 'x': # exits loop if user enters 'x'
+        break
+    elif userInput in dictionary: # if it is in the dictionary, it prints
+        print("The definition of %s is:" % userInput) # prints the shortcut entered by the user
+        print(dictionary.get(userInput)) # prints the definition of the shortcut entered by the user.
+    else:
+        print("%s is not in the dictionary" % userInput) # if not, prints out error message
