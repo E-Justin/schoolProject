32eb2189f8fa3e864a1093acb82ced59a3e0ff38
E-Justin <67754579+E-Justin@users.noreply.github.com>
2021-04-16 06:32:35 -0500

    Update rangeGame.py

--- rangeGame.py
+++ rangeGame.py
@@ -1,8 +1,6 @@
 """ Remember the random number - high/low game from way back at the beginning of the semester?  Well, we are playing it again, only with a GUI.
 Create an application that generates a random number within a range specified by the user, 
 
-
-
 then, using prompter boxes, allows the user to guess 
 the hidden number.  When the correct guess is picked, the user wins and the number of tries and the correct number should be displayed on the 
 original window.
@@ -13,9 +11,9 @@ User interface design is your call.  Make sure that the directions are clear to 
 import random
 from breezypythongui import EasyFrame
 
-#globals
-win = 0
-loss = 0
+
+global tries
+tries = 0
 
 
 
@@ -26,25 +24,30 @@ def generateRandomNumber(a,b):
 class rangeGame(EasyFrame):
     """Illustrates command buttons and user events."""
 
+    
+
     def __init__(self):
         """Sets up the window, label, and buttons."""
         EasyFrame.__init__(self, title = "Range Game")
 
+        # to keep track of number of tries
+        self.count = 0
+
         # Label and field for the input
-        self.addLabel(text = "Range: Low",
+        self.addLabel(text = "Range: Low\n (lowest number possible)",
                       row = 1, column = 0)
         self.rangeLow = self.addTextField(text = "",
                                             row = 1,
                                             column = 1)
 
-        self.addLabel(text = "Range: High",
+        self.addLabel(text = "Range: High\n (Highest number possible)",
                       row = 1, column = 2)
         self.rangeHigh = self.addTextField(text = "",
                                             row = 1,
                                             column = 3)
 
         # Label and field for the output
-        self.addLabel(text = "Output",
+        self.addLabel(text = "Random Number = ",
                       row = 1, column = 4)
         self.outputField = self.addTextField(text = "",
                                              row = 1,
@@ -52,15 +55,13 @@ class rangeGame(EasyFrame):
                                              state = "readonly")
 
         
-       
-
         # Label to display wins
-        self.label = self.addLabel(text = " Wins: " + str(win), row = 0, column = 0, columnspan = 1)
+        #self.label = self.addLabel(text = " Tries: " + str(self.count), row = 0, column = 0, columnspan = 1)
         # label to display losses
-        self.label = self.addLabel(text = " Losses: " + str(win), row = 0, column = 1, columnspan = 1)
+        #self.label = self.addLabel(text = " Losses: %d" % loss, row = 0, column = 1, columnspan = 1)
 
         # button to generate a random number.
-        self.randomNumber = self.addButton(text = "Random Number",
+        self.randomNumber = self.addButton(text = "Generate random Number",
                                        row = 2, column = 1,
                                        command = self.randomNumber)
         # button to allow user to guess the outcome
@@ -72,14 +73,12 @@ class rangeGame(EasyFrame):
     # prompter box
     def guess(self):
         text = self.prompterBox(title = "Input Dialog",promptString = "What is your guess?:")
-        self.label = self.addLabel(text = "Your guess = " + text, row = 0, column = 3)
+        self.label = self.addLabel(text = "Your guess = " + text, row = 0, column = 1)
         
         global guessedNumber
         guessedNumber = text
         
     
-    
-
     # Methods to handle user events.
     def randomNumber(self):
         global result
@@ -89,11 +88,13 @@ class rangeGame(EasyFrame):
         b = int(self.rangeHigh.get())
         result = generateRandomNumber(a,b)
         self.outputField.setText(result)
+        self.count += 1
+        self.label["text"] = "Tries: " + str(self.count)
+        if int(guessedNumber) == result:
+            self.label["text"] = "You are a Winner!!! And it only took %d tries" % self.count
+            self.count = 0
 
-    
-
-    
-  
+        
 
 def main():
     """Instantiate and pop up the window."""
