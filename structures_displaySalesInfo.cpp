592be9a5c19389d31574b4edbceffe5d02e409b4
E-Justin <67754579+E-Justin@users.noreply.github.com>
2021-10-01 11:17:11 -0500

    Update structures_displaySalesInfo.cpp

--- structures_displaySalesInfo.cpp
+++ structures_displaySalesInfo.cpp
@@ -1,3 +1,4 @@
+
 #include <iomanip>  // input/ output library
 #include <string> // to use strings
 #include <iostream> // cin and cout..
@@ -20,8 +21,8 @@ struct Sales
 
 Sales person1 =			// for testing purposes only
 {
-	12345,
-	"Lunch Box",
+	1001,
+	"Foam Padding",
 	12,
 	123.49,
 	123456789,
@@ -41,24 +42,81 @@ void displaySalesData(Sales x) // method to display sales info
 	cout << "User name: " << x.userName << endl;
 	cout << "Expiration Date MMYY: " << x.expirationDate << endl;
 	cout << "Security Code: " << x.securityCode << endl;
-	if (x.isValid == 0) // if isValid == false
-	{
-		cout << "Not Valid " << endl;
-	}
-	else // if isValid == true
-	{
-		cout << "Valid " << endl;
-	}
+	
 
 	return;
+}
+
+void preCheck(Sales &x) // passing a strucutre by reference as an argument 
+{
+	
+
+	if (x.itemNumber == 1001)
+	{
+		x.isValid = true;
+		x.itemName = "Foam Padding";
+		// item cost == $8.50
+		x.totalCost = (x.itemQuantity * 8.50);
+	}
+	if (x.itemNumber == 1002)
+	{
+		x.isValid = true;
+		x.itemName = "Box Tape";
+		// item cost == $10.11
+		x.totalCost = (x.itemQuantity * 10.11);
+	}
+	if (x.itemNumber == 1003)
+	{
+		x.isValid = true;
+		x.itemName = "Moving Blanket";
+		// item cost == $20.00
+		x.totalCost = (x.itemQuantity * 20.00);
+	}
+	if (x.itemNumber == 1004)
+	{
+		x.isValid = true;
+		x.itemName = "Moving Box";
+		// item cost == $4.25
+		x.totalCost = (x.itemQuantity * 4.25);
+	}
 }
 
 
 
 int main()
 {
-	displaySalesData(person1);
 	
+	cout << boolalpha; // step 1
+
+	Sales inputData; //step 2
+
+	cout << "What is the item number?" << endl;  // step 3
+	cin >> inputData.itemNumber;
+	cout << "What is the item quantity?" << endl;
+	cin >> inputData.itemQuantity;
+
+	preCheck(inputData);
+
+	if (inputData.isValid == 0) // if isValid == false
+	{
+		cout << "Not Valid " << endl;
+	}
+	else // if isValid == true
+	{
+		cout << "Valid " << endl;
+		cout << "What is your credit card number? " << endl;
+		cin >> inputData.cardNumber;
+		cout << "What is your user name? " << endl;
+		cin.ignore();
+		getline(cin, inputData.userName); 
+		cout << "What is your credit card's expiration date? " << endl;
+		cin >> inputData.expirationDate;
+		cout << "What is your credit cards's security code? " << endl;
+		cin >> inputData.securityCode;
+
+	}
+
+	displaySalesData(inputData);
 	
 	return 0;
 }
