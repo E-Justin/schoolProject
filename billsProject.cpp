450a9f4da0f82c4d1a2aa991cd70e46eaee5297b
E-Justin <67754579+E-Justin@users.noreply.github.com>
2021-10-04 13:18:40 -0500

    Update billsProject.cpp
    method works and is now personalized.
    Still have to add the other features

--- billsProject.cpp
+++ billsProject.cpp
@@ -26,6 +26,7 @@ struct Bills
 	string otherIs; // to let user input what the other bill is
 	bool paidOther = false; //will be set to true if they paid 'other'
 	double total = 0;
+	string name;
 };
 Bills person1, person2, person3, person4, person5;
 
@@ -33,57 +34,61 @@ Bills person1, person2, person3, person4, person5;
 
 Bills perPersonTotal(Bills personNumber)
 {
+	cout << "What is the person's name that you are about to enter bill amounts for?  ";
+	cin >> personNumber.name;
+
 	while (selection != "0")
 	{
 		cout << " ********** Bills Menu ********** " << endl;
-		cout << " Select from the following to add an amount for this person" << endl;
+		cout << " Select from the following to add a bill paid by "<< personNumber.name << endl;
 		cout << "1 : Water Bill " << endl;
 		cout << "2 : Rent/ Mortgage " << endl;
 		cout << "3 : Electric Bill " << endl;
 		cout << "4 : Groceries " << endl;
 		cout << "5 : Insurance " << endl;
 		cout << "6 : Other " << endl;
-		cout << "0 : Exit this person's bills  ";
+		cout << "0 : Exit " << personNumber.name << "'s bills  ";
 
 		cin >> selection;
 
 		if (selection == "1") // water
 		{
-			cout << "How much did this person pay for water this month ?";
+			cout << "How much did " << personNumber.name <<" pay for water this month ?";
 			cin >> personNumber.water;
 		}
 		if (selection == "2") // rent/ mortgage
 		{
-			cout << "How much did this person pay for water this month ?";
+			cout << "How much did " << personNumber.name << " pay for water this month ?";
 			cin >> personNumber.rent;
 		}
 		if (selection == "3") // electricity
 		{
-			cout << "How much did this person pay for Electricity this month ?";
+			cout << "How much did " << personNumber.name << " pay for Electricity this month ?";
 			cin >> personNumber.electricity;
 		}
 		if (selection == "4") // groceries
 		{
-			cout << "How much did this person pay for water this month ?";
+			cout << "How much did " << personNumber.name << " pay for water this month ?";
 			cin >> personNumber.rent;
 		}
 		if (selection == "5") // insurance
 		{
-			cout << "How much did this person pay for insurance this month ?";
+			cout << "How much did " << personNumber.name << " pay for insurance this month ?";
 			cin >> personNumber.insurance;
 		}
 		if (selection == "6") // other
 		{
-			cout << "What bill did they pay that is not listed?  ";
-			cin >> personNumber.otherIs;
+			cout << "What bill did " << personNumber.name << " pay that is not listed?  ";
+			cin.ignore();
+			getline(cin, personNumber.otherIs); // user inputs the name of the other bill that is not listed in the menu
 			personNumber.paidOther = true;
-			cout << "How much did this person pay for " << personNumber.otherIs << " this month ?";
-			cin >> personNumber.insurance;
+			cout << "How much did " << personNumber.name << " pay for " << personNumber.otherIs << " this month ?";
+			cin >> personNumber.other;
 		}
 
 	}
 	personNumber.total = personNumber.water + personNumber.rent + personNumber.electricity + personNumber.groceries + personNumber.other;
-	cout << "This person's total is : $" << personNumber.total << endl << endl;
+	cout << endl << personNumber.name << "'s total is : $" << personNumber.total << endl << endl;
 	return personNumber;
 
 }
