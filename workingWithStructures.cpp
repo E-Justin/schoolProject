2603ba218884031493fbce559c0957e1712a7a0c
E-Justin <67754579+E-Justin@users.noreply.github.com>
2021-10-04 11:44:20 -0500

    Update workingWithStructures.cpp

--- workingWithStructures.cpp
+++ workingWithStructures.cpp
@@ -6,48 +6,78 @@
 #include <cctype>
 using namespace std;
 
+char separator; // used in formatting date and time ex '/' or ':'
+
 struct Date
 {
 	int month;
 	int day;
 	int year;
 };
+Date DateOfBirth; // struct variable to hold user DOB
 
 struct TimeOfDay
 {
 	int hour;
 	int minute;
-	
+
 };
+
+TimeOfDay WakeUpTime;
+
+
+struct DateTime
+{
+	Date AppointmentDate;
+	TimeOfDay AppointmentTime;
+};
+
+DateTime AppointmentInfo;
+
+
+Date loadAndReturnDate() // loads and returns DOB
+{
+
+	cout << "What is your date of birth? MM/DD/YYYY" << endl;
+	cin >> DateOfBirth.month >> separator >> DateOfBirth.day >> separator >> DateOfBirth.year;
+
+	cout << "Birth Date: " << DateOfBirth.month << "/" << DateOfBirth.day << "/" << DateOfBirth.year << endl;
+
+	return DateOfBirth;
+}
+Date AppointmentDate;
+
+TimeOfDay loadAndReturnTimeOfDay() // loads and returns wake up time
+{
+	cout << "What time do you typically wake up? HH:MM" << endl;
+	cin >> WakeUpTime.hour >> separator >> WakeUpTime.minute;
+
+	cout << "Wake up time : " << WakeUpTime.hour << ":" << WakeUpTime.minute << endl;
+
+	return WakeUpTime;
+}
+TimeOfDay AppointmentTime;
+
+DateTime loadAndReturnAppointmentInfo() // loads and returns appointment info
+{
+	cout << "What is the date of your next dentist appointment? MM/DD/YYYY " << endl;
+	cin >> AppointmentInfo.AppointmentDate.month >> separator >> AppointmentInfo.AppointmentDate.day >> separator >> AppointmentInfo.AppointmentDate.year;
+
+	cout << "The date of your next dentist appointment is : " << AppointmentInfo.AppointmentDate.month << "/" << AppointmentInfo.AppointmentDate.day << "/" << AppointmentInfo.AppointmentDate.year << endl;
+
+	return AppointmentInfo;
+}
 
 int main()
 {
-	Date dateOfBirth; // struct variable to hold user DOB
-	Date nextAppointmentDate; // struct variable to hold user's next dentist appointment
-
-	TimeOfDay wakeUpTime;
-	TimeOfDay nextAppointmentTime;
-
-	char separator; 
-
-	cout << "What is your date of birth? MM/DD/YYYY" << endl;
-	cin >> dateOfBirth.month >> separator >> dateOfBirth.day >> separator >> dateOfBirth.year;
-
-	cout << "What time do you typically wake up? HH:MM" << endl;
-	cin >> wakeUpTime.hour >> separator >> wakeUpTime.minute;
-
-	cout << "When is the date of your next dentist appointment? MM/DD/YYYY" << endl;
-	cin >> nextAppointmentDate.month >> separator >> nextAppointmentDate.day >> separator >> nextAppointmentDate.year;
-
-	cout << "What time is your next dentist appointment? HH:MM" << endl;
-	cin >> nextAppointmentTime.hour >> separator >> nextAppointmentTime.minute;
-
-	
-
 	
 	
-
-
+	loadAndReturnDate();
+	
+	loadAndReturnTimeOfDay();
+	
+	loadAndReturnAppointmentInfo();
+	
 
 	return 0;
 }
