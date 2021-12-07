a84b3f25cf0928d8415f0c547339aca118e95b9f
E-Justin <67754579+E-Justin@users.noreply.github.com>
2021-12-07 06:38:41 -0600

    Update fizzBuzz.py
    cleaned up code. Turned it in to one method

--- fizzBuzz.py
+++ fizzBuzz.py
@@ -1,24 +1,32 @@
+# fizzBuzz
+# checks all numbers from 1 - the number entered by the user
+# fizz = number is divisible by 3
+# buzz = number is divisible by 5
+# fizzBuzz = number is divisible by both 3 and 5
 highNum = int(input("Please enter an integer: This program will check all numbers from 0 to _____"))
 
-index = 1
-threeAndFive = "FizzBuzz" # if divisible by three and five
-three = "Fizz" # if divisible by three
-five = "Buzz" # if divisible by five
 
 
-while index <= highNum:
-    if index % 3 == 0 and index % 5 ==0: # if divisible by both 3 and 5
-        print(index, ' : ', threeAndFive)
-        index = index + 1
+def fizzBuzz(highNum):
+    index = 1
+    threeAndFive = "FizzBuzz" # if number is divisible by three and five
+    three = "Fizz " # if number is divisible by 3, but not 5
+    five = "Buzz " # if number is divisible by 5, but not 3
+    while index <= highNum:
         
-    elif index % 3 == 0 and index % 5 !=0: # if divisible by 3 only
-        print(index, ' : ',three)
-        index = index + 1
-        
-    elif index % 5 == 0 and index % 3 != 0: # if divisible by 5 only
-        print(index, ' : ',five)
-        index = index + 1
-        
-    else:
-        print(index) # prints the index integer only if not divisible by 3 and/ or 5
-        index = index + 1
+        if index % 3 == 0 and index % 5 == 0:
+            print (index, " : " , threeAndFive)
+            index += 1
+        elif index % 3 == 0:
+            print(index, " : " , three)
+            index += 1
+        elif index % 5 == 0:
+            print (index, " : " , five)
+            index += 1
+        else:
+            print (index)
+            index += 1
+   
+  
+fizzBuzz(highNum);        
+    
